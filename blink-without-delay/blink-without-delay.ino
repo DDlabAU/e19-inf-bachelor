8bc313225e7a7c36065c7c021ddbc93628ac3b73
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2019-09-26 15:08:55 +0200

    Add example code

--- blink-without-delay/blink-without-delay.ino
+++ blink-without-delay/blink-without-delay.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin;
+boolean ledVal = HIGH;
+int currentTime;
+int lastChange = 0;
+int wait = 500;
 
+void setup() {
+  pinMode(ledPin, ledVal);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  currentTime = millis();
 
+  if(currentTime > lastChange + wait) {
+    if(ledVal == HIGH) {
+      ledVal = LOW;
+    } else {
+      ledVal = HIGH;
+    }
+    // The above if-statement can also be accomplished with
+    // this line of code:
+    // ledVal = !ledVal;
+
+    lastChange = currentTime;
+  }
+  
+  digitalWrite(ledPin, ledVal);
 }
