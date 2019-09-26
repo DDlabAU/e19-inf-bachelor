1fff2b28606a7f22cb816f540edd8696cac3fe2c
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2019-09-26 15:48:30 +0200

    Add example code

--- analog-input/analog-input.ino
+++ analog-input/analog-input.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 6;
+int ledVal = 0;
 
+int ldrPin = A0;
+int ldrVal;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ldrVal = analogRead(ldrPin);
 
+  Serial.print("LDR value: ");
+  Serial.println(ldrVal);
+
+  int ldrVal = constrain(ldrVal, 100, 900);
+
+  Serial.print("Constrained value: ");
+  Serial.println(ldrVal);
+
+  int mappedVal = map(constrainedVal, 100, 900, 0, 255);
+  Serial.print("Value mapped from 100-900 to 0-255: ");
+  Serial.println(mappedVal);
+
+  analogWrite(ledPin, mappedVal);
 }
