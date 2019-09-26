481fa5e681f9e525afa23f3ba6d0a28dd0efd2c9
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2019-09-26 15:16:18 +0200

    Add example code

--- digital-input/digital-input.ino
+++ digital-input/digital-input.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 6;
+boolean ledVal = LOW;
 
+int buttonPin;
+boolean buttonVal = HIGH;
+boolean prevButtonVal = HIGH;
+
+void setup() {
+  pinMode(ledPin, INPUT);
+
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  prevButtonVal = buttonVal;
+  buttonVal = digitalRead(buttonPin);
 
+  if(buttonVal == LOW && prevButtonVal == HIGH) {
+    if(ledVal == HIGH) {
+      ledVal = LOW;
+    } else {
+      ledVal = HIGH;
+    }
+    // The above if-statement can also be accomplished with
+    // this line of code:
+    // ledVal = !ledVal;
+  }
 }
